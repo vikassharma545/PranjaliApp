d726be8007fd09dfab929e3cda964e49ac9439dd
Vikas Sharma <jnv2252@gmail.com>
2024-10-21 09:09:56 +0530

    handling error 404

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, make_response
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
     return render_template("index.html")
+
+@app.errorhandler(404)
+def page_not_found(error):
+    return make_response(render_template('error404.html'), 404)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
